8be0ccd25fdcb2e2418c0d49aa16e246f92036fe
Yash <titanmattersbot05@gmail.com>
2024-12-29 12:51:08 +0530

    Update send_restart_message.py

--- plugins/send_restart_message.py
+++ plugins/send_restart_message.py
@@ -1,30 +1,25 @@
 
 import logging
-from telegram import Bot
+from bot import Bot
 import datetime
-
+from pyrogram import Client, filters
 
 # Replace 'LOG_CHANNEL_ID' with your log channel's ID
 LOG_CHANNEL_ID = '-1002132998073'
 
 
-def send_restart_message():
-    # Current date and time
+RESTART_TXT = """
+Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
+
+📅 Dᴀᴛᴇ : {}
+⏰ Tɪᴍᴇ : {}
+🌐 Tɪᴍᴇᴢᴏɴᴇ : Asia/Kolkata
+🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: v2.7.1 [ Sᴛᴀʙʟᴇ ]"""
+
+@Client.on_message(filters.command("restart")) # You will use /restart command to trigger this event.
+async def send_restart_message(client, message):
     now = datetime.datetime.now()
     date_str = now.strftime("%Y-%m-%d")
     time_str = now.strftime("%H:%M:%S")
-
-    RESTART_TXT = f"""
-Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
-
-📅 Dᴀᴛᴇ : {date_str}
-⏰ Tɪᴍᴇ : {time_str}
-🌐 Tɪᴍᴇᴢᴏɴᴇ : Asia/Kolkata
-🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: v2.7.1 [ Sᴛᴀʙʟᴇ ]
-"""
-
-    # Send the message to the log channel
-    bot.send_message(chat_id=LOG_CHANNEL_ID, text=RESTART_TXT, parse_mode='HTML')
-
-if __name__ == "__main__":
-    send_restart_message()
+    formatted_text = RESTART_TXT.format(date_str, time_str)
+    await client.send_message(message.chat.id, formatted_text)
