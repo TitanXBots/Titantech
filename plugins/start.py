6999a89993217bbed496dde35b23be432a297eb2
Yash <titanmattersbot05@gmail.com>
2024-12-31 21:06:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -243,10 +243,6 @@ async def delete_files(messages, client, k):
     command_part = k.command[1] if k.command and len(k.command) > 1 else None
 
     if command_part:
-        try:
-  #code
-        except Exception as e:
-            print(e)
         button_url = f"https://t.me/{client.username}?start={command_part}"
         keyboard = InlineKeyboardMarkup(
             [
