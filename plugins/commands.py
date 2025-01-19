ad3922fd821f6c98b696c638daa28583b36235f4
Yash <titanmattersbot05@gmail.com>
2025-01-19 14:57:34 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,40 @@
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import ADMINS
+
+# A set to hold banned user IDs
+banned_users = set()
+
+# Command to ban a user
+@Client.on_message(filters.command("ban") & filters.user("ADMINS"))
+def ban_user(client, message: Message):
+    if len(message.command) < 2:
+        message.reply_text("Usage: /ban ")
+        return
+
+    user_id = int(message.command[1])
+    banned_users.add(user_id)
+    message.reply_text(f"User {user_id} has been banned.")
+
+# Command to unban a user
+@Client.on_message(filters.command("unban") & filters.user("ADMINS"))
+def unban_user(client, message: Message):
+    if len(message.command) < 2:
+        message.reply_text("Usage: /unban ")
+        return
+
+    user_id = int(message.command[1])
+    if user_id in banned_users:
+        banned_users.remove(user_id)
+        message.reply_text(f"User {user_id} has been unbanned.")
+    else:
+        message.reply_text(f"User {user_id} is not banned.")
+
+# Check if the user is banned before processing commands
+@Client.on_message(filters.command("start")
+def check_ban(client, message: Message):
+    user_id = message.from_user.id
+    if user_id in banned_users:
+        message.reply_text("You are banned from using this bot.")
+    
