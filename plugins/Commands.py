9adc29ed3e76673ed13bfdb6d335539e351f7a64
Yash <titanmattersbot05@gmail.com>
2025-01-24 10:34:49 +0530

    Update Commands.py

--- plugins/Commands.py
+++ plugins/Commands.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-@Bot.on_message(filters.command("id"))
+@Client.on_message(filters.command("id"))
 async def id_command(client: Client, message: Message):
     if message.chat.type == "private":
        await message.reply_text(f"Your user ID is : {message.from_user.id}")
