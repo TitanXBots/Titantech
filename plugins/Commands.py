91cb87a9dc641fcf318351be49075f59809bce69
Yash <titanmattersbot05@gmail.com>
2025-01-24 10:45:34 +0530

    Update Commands.py

--- plugins/Commands.py
+++ plugins/Commands.py
@@ -6,8 +6,7 @@ from pyrogram.types import Message
 async def id_command(client: Client, message: Message):
     if message.chat.type == "private":
        await message.reply_text(f"Your user ID is : {message.from_user.id}")
-    else:
-       await message.reply_text(f"Chat ID is : {message.chat.id}")
+   
 
 
 
