5bd8e50b48e06f53671604fedc6d438f9dbe9fe3
Yash <titanmattersbot05@gmail.com>
2025-02-04 22:09:57 +0530

    Add files via upload

---
+++ plugins/commands.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+@Client.on_message(filters.command("id"))
+async def id_command(client: Client, message: Message):
+    if message.chat.type == "private":
+       await message.reply_text(f"Your user ID is : {message.from_user.id}")
+    else:
+       await message.reply_text(f"Your user ID is : {message.chat.id}")
+    
+        
+   
+
+
+
+# --- Bot Run ---
+if __name__ == "__main__":
+    print("Bot Started...")
