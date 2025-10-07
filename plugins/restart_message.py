c6b96ce9f92f0efb8bdf6dfb06c33f98fc3eb8db
Yash <titanmattersbot05@gmail.com>
2025-10-07 21:03:22 +0530

    Update restart_message.py

--- plugins/restart_message.py
+++ plugins/restart_message.py
@@ -8,14 +8,7 @@ from config import ADMINS, LOG_CHANNEL_ID, RESTART_TXT
 
 
 
-RESTART_TXT = """
-Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
 
-📅 Dᴀᴛᴇ : {date}
-⏰ Tɪᴍᴇ : {time}
-🌐 Tɪᴍᴇᴢᴏɴᴇ : Asia/Kolkata
-🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: v2.7.1 [ Sᴛᴀʙʟᴇ ]
-"""
 
 # Setup Logging
 logging.basicConfig(
